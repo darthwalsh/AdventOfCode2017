698ba0f97d05b1d4384893b4da46f8c9b6189c69
Carl Walsh <darthwalsh@gmail.com>
2017-12-02 21:01:14 -0800

    finished 2

--- 2.ps1
+++ 2.ps1
@@ -1,9 +1,7 @@
-$x = "5 9 2 8
-9 4 7 3
-3 8 6 5" -split '\r\n'
-#$x = gc .\day2.txt
+#$x = @("5 9 2 8", "9 4 7 3", "3 8 6 5")
+$x = gc .\day2.txt
 
 $x | % { $nums = $_ -split '\s' | % { [int]$_ }; ($nums | measure -Max | select -expand "Maximum" ) - ($nums | measure -Minimum | select -expand "Minimum") } `
     | measure -sum | select -expand "sum"
   
-$x | % { $nums = $_ -split '\s' | % { [int]$_ } } | % { $i = $_; $y | ? { $_ -gt $i -and $_ % $i -eq 0} | % { $_ / $i } }
+$x | % { $nums = $_ -split '\s' | % { [int]$_ }; $nums | % { $i = $_; $nums | ? { $_ -gt $i -and $_ % $i -eq 0} | % { $_ / $i } } } | measure -sum | select -expand "sum"
